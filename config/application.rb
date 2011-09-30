16b2daa82d4559090985e8e48e48ce09762e1f56
Karunakar (Ruby) <revurikarna@gmail.com>
2011-09-30 10:31:21 +0530

    config changes

--- config/application.rb
+++ config/application.rb
@@ -41,8 +41,8 @@ module RailsOffline
     config.filter_parameters += [:password]
 
     # Enable the asset pipeline
-    config.assets.enabled = true
-
+    config.assets.enabled = false
+    #config.cache_classes = false
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
