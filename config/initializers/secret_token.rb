8f4411d6f0decae89291d1631c5cde1bec8d46a1
Karunakar (Ruby) <revurikarna@gmail.com>
2011-09-30 08:08:56 +0530

    rails offline app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsOffline::Application.config.secret_token = 'f4392f08fa2b21b29f33fb1d4d519a6680dcd69ace8575a242d3fea32b4f431e6321baae2d462e98eb48c1fc729100ec6f19e6521b3bbe1bd006744a51107c0b'
